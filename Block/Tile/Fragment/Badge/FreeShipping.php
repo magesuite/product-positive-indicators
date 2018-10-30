4a6ca8cd300f6f5391c6eac4c05342977b80c81f
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2018-10-30 17:10:08 +0100

    Moved positive indicator badges to this extension

---
+++ Block/Tile/Fragment/Badge/FreeShipping.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MageSuite\ProductPositiveIndicators\Block\Tile\Fragment\Badge;
+
+class FreeShipping implements \MageSuite\ProductTile\Block\Tile\Fragment\BadgeInterface
+{
+    /**
+     * @var \MageSuite\ProductPositiveIndicators\Helper\Product
+     */
+    protected $productHelper;
+
+    public function __construct(\MageSuite\ProductPositiveIndicators\Helper\Product $productHelper)
+    {
+        $this->productHelper = $productHelper;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isVisible(\MageSuite\ProductTile\Block\Tile $tile)
+    {
+        return $this->productHelper->showFreeShippingInProductTiles() and $this->productHelper->isFreeShipped($tile->getProductEntity());
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue(\MageSuite\ProductTile\Block\Tile $tile)
+    {
+        return '';
+    }
+
+    /**
+     * @return string
+     */
+    public function getCssModifier(\MageSuite\ProductTile\Block\Tile $tile)
+    {
+        return '';
+    }
+}
