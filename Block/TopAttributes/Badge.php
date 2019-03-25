db418894d70f6d8f00bdef321a1ba8fee3c495c4
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2019-03-25 11:07:01 +0100

    Top Attributes Badge

---
+++ Block/TopAttributes/Badge.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MageSuite\ProductPositiveIndicators\Block\TopAttributes;
+
+class Badge extends \Magento\Framework\View\Element\Template
+{
+    protected $_template = 'MageSuite_ProductPositiveIndicators::topattributes/badge.phtml';
+
+}
---
+++ Plugin/Catalog/Block/Product/View/Attributes/AddTopAttributeBadge.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace MageSuite\ProductPositiveIndicators\Plugin\Catalog\Block\Product\View\Attributes;
+
+class AddTopAttributeBadge
+{
+    /**
+     * @var \Magento\Framework\View\Element\BlockFactory
+     */
+    protected $blockFactory;
+
+    /**
+     * @var \MageSuite\ProductPositiveIndicators\Helper\TopAttribute
+     */
+    protected $topAttributeHelper;
+
+    /**
+     * @var \MageSuite\ProductPositiveIndicators\Helper\Configuration\TopAttributes
+     */
+    protected $topAttributesConfiguration;
+
+    public function __construct(
+        \Magento\Framework\View\Element\BlockFactory $blockFactory,
+        \MageSuite\ProductPositiveIndicators\Helper\TopAttribute $topAttributeHelper,
+        \MageSuite\ProductPositiveIndicators\Helper\Configuration\TopAttributes $topAttributesConfiguration
+    ){
+        $this->blockFactory = $blockFactory;
+        $this->topAttributeHelper = $topAttributeHelper;
+        $this->topAttributesConfiguration = $topAttributesConfiguration;
+    }
+
+    public function afterGetAdditionalData($subject, $result)
+    {
+        if(!$this->topAttributesConfiguration->isEnabled()){
+            return $result;
+        }
+
+        $topAttributesBlockHtml = $this->blockFactory->createBlock(\MageSuite\ProductPositiveIndicators\Block\TopAttributes\Badge::class)->toHtml();
+
+        foreach($result as $attributeCode => $data){
+
+            if(!$this->topAttributeHelper->checkTopAttribute($attributeCode, $data['value'])){
+                continue;
+            }
+
+            $result[$attributeCode]['value'] = $this->addTopAttributeBadge($result[$attributeCode]['value'], $topAttributesBlockHtml);
+        }
+
+        return $result;
+    }
+
+    protected function addTopAttributeBadge($value, $badgeHtml)
+    {
+        return $value . $badgeHtml;
+    }
+}
