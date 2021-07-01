6e58923d4fe5a477d38c648537093eff95b27841
Piotr Matras <piotr.matras@creativestyle.pl>
2021-07-01 10:27:21 +0200

    feat: MGP-187 optimize autocomplete action

--- Plugin/AddIndicatorsToSearchFlyout.php
+++ Plugin/AddIndicatorsToSearchFlyout.php
@@ -1,43 +1,35 @@
 <?php
+declare(strict_types=1);
 
 namespace MageSuite\ProductPositiveIndicators\Plugin;
 
 class AddIndicatorsToSearchFlyout
 {
     /**
-     * @var \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
-     */
-    private $productRepository;
-
-    /**
      * @var \MageSuite\ProductPositiveIndicators\Service\FreeShipping
      */
-    private $freeShipping;
+    protected $freeShipping;
 
-    /**
-     * AddPopularBadgeToSearchFlyout constructor.
-     * @param \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
-     */
     public function __construct(
-        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
         \MageSuite\ProductPositiveIndicators\Service\FreeShipping $freeShipping
     ){
-        $this->productRepository = $productRepository;
         $this->freeShipping = $freeShipping;
     }
 
-    public function afterCreate(\Smile\ElasticsuiteCatalog\Model\Autocomplete\Product\ItemFactory $subject, $result, array $data)
-    {
-        $product = $this->productRepository->get($data['product']->getSku());
+    public function beforeCreate(
+        \Smile\ElasticsuiteCatalog\Model\Autocomplete\Product\ItemFactory $subject,
+        array $data
+    ): array {
+        $product = $data['product'];
 
         if ($product->getPopularIcon()) {
-            $result->setData('popular_icon', __('Popular'));
+            $data['popular_icon'] = __('Popular');
         }
 
-        if($this->freeShipping->showInSearchAutosuggest() and $this->freeShipping->isFreeShipped($product)){
-            $result->setData('free_shipping', __('Free Shipping'));
+        if ($this->freeShipping->showInSearchAutosuggest() && $this->freeShipping->isFreeShipped($product)) {
+            $data['free_shipping'] = __('Free Shipping');
         }
 
-        return $result;
+        return [$data];
     }
 }
