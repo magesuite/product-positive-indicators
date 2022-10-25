c4355abff14a5ead6c2c50175cd679bdd9a7e967
Marta Siewierska-Stańczak <marta.siewierska-stanczak@creativestyle.pl>
2022-10-25 14:19:20 +0200

    fix: [LEDS-2301] Show popualar badge for all places outside category list

--- view/frontend/web/js/tile-badge-shower.js
+++ view/frontend/web/js/tile-badge-shower.js
@@ -1,8 +1,12 @@
 /**
  * Handle popular badge showing/hiding
  * Check if the current location is category page
+ * For all pages that are not category page, always how popular badge (f.e. wishlist, carousels, autocomplete)
+ * If the current page is category page first check if a badge is part of a product that is on products list (one of
+ * its parents has product-items class)
  * Then for every popular badge check in which categories this product is popular(data-visible-in-categories)
  * Show popular badge only if there is a match between current category and category in which given product is popular
+ * For all other components with tile on category page, show popular badge without any check.
  */
 
 define([
@@ -13,10 +17,26 @@ define([
     return function (config) {
         if (config.isCurrentPageCategory) {
             document.querySelectorAll('.cs-product-tile__badge--popular').forEach((badge) => {
-                const categories = JSON.parse(badge.getAttribute('data-visible-in-categories'));
-                if (categories.includes(config.currentCategoryId)) {
+
+                function hasSomeParentTheClass(element, classname) {
+                    if (element.className.split(' ').indexOf(classname) >= 0) return true;
+                    return element.parentNode && hasSomeParentTheClass(element.parentNode, classname);
+                }
+
+                const isOnCategoryList = hasSomeParentTheClass(badge, 'product-items');
+
+                if (isOnCategoryList) {
+                    const categories = JSON.parse(badge.getAttribute('data-visible-in-categories'));
+                    if (categories.includes(config.currentCategoryId)) {
+                        badge.style.display = '';
+                    }
+                } else {
                     badge.style.display = '';
                 }
+            });
+        } else {
+            document.querySelectorAll('.cs-product-tile__badge--popular').forEach((badge) => {
+                badge.style.display = '';
             });
         }
     }
