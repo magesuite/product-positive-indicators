f6d548edbc732c1a3cf72a4f2e26dbe50ab0b410
pre-commit fix Paweł Ledwig <pawel.ledwig@creativestyle.pl>
2023-05-22 12:25:11 +0000

    auto fixes from pre-commit CI job
    job url: https://gitlab.creativestyle.pl/magesuite/product-positive-indicators/-/jobs/460104

--- Test/Integration/_files/products_rollback.php
+++ Test/Integration/_files/products_rollback.php
@@ -8,7 +8,7 @@ foreach ([600,601,602,603,604] as $productId) {
     $product = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create('Magento\Catalog\Model\Product');
 
     $product->load($productId);
-    
+
     if ($product->getId()) {
         $product->delete();
     }
