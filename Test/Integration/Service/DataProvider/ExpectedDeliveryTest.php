900a8592566b7c78a1062fce5061c16562a4cebb
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2026-01-22 11:39:31 +0100

    chore: Fix PHPUnit deprecations

--- Test/Integration/Service/DataProvider/ExpectedDeliveryTest.php
+++ Test/Integration/Service/DataProvider/ExpectedDeliveryTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MageSuite\ProductPositiveIndicators\Test\Integration\Service\DataProvider;
 
 /**
@@ -8,20 +10,9 @@ namespace MageSuite\ProductPositiveIndicators\Test\Integration\Service\DataProvi
  */
 class ExpectedDeliveryTest extends \PHPUnit\Framework\TestCase
 {
-    /**
-     * @var \Magento\Catalog\Api\ProductRepositoryInterface
-     */
-    protected $productRepository;
-
-    /**
-     * @var \MageSuite\ProductPositiveIndicators\Helper\Configuration\ExpectedDelivery
-     */
-    protected $configuration;
-
-    /**
-     * @var \MageSuite\ProductPositiveIndicators\Service\DataProvider\ExpectedDelivery
-     */
-    protected $expectedDeliveryDataProvider;
+    protected ?\Magento\Catalog\Api\ProductRepositoryInterface $productRepository;
+    protected ?\MageSuite\ProductPositiveIndicators\Helper\Configuration\ExpectedDelivery $configuration;
+    protected ?\MageSuite\ProductPositiveIndicators\Service\DataProvider\ExpectedDelivery $expectedDeliveryDataProvider;
 
     protected function setUp(): void
     {
@@ -38,12 +29,9 @@ class ExpectedDeliveryTest extends \PHPUnit\Framework\TestCase
      * @magentoAppIsolation enabled
      * @magentoDbIsolation enabled
      * @magentoDataFixture loadProducts
-     * @param array $config
-     * @param string $sku
-     * @param array $excepted
      * @dataProvider dataProvider
      */
-    public function testItReturnsCorrectData($config, $sku, $excepted)
+    public function testItReturnsCorrectData(array $config, string $sku, ?array $expected): void
     {
         $product = $this->productRepository->get($sku);
 
@@ -51,11 +39,11 @@ class ExpectedDeliveryTest extends \PHPUnit\Framework\TestCase
 
         $deliveryData = $this->expectedDeliveryDataProvider->getDeliveryData($product);
 
-        if ($excepted === null) {
+        if ($expected === null) {
             $this->assertNull($deliveryData);
         } else {
-            $this->assertEquals($excepted['shipDayName'], (string)$deliveryData->getShipDayName());
-            $this->assertEquals($excepted['nextShipDayName'], (string)$deliveryData->getNextShipDayName());
+            $this->assertEquals($expected['shipDayName'], (string)$deliveryData->getShipDayName());
+            $this->assertEquals($expected['nextShipDayName'], (string)$deliveryData->getNextShipDayName());
         }
     }
 
@@ -69,7 +57,7 @@ class ExpectedDeliveryTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals(8, $result);
     }
 
-    private function prepareConfiguration($testConfig)
+    protected function prepareConfiguration(array $testConfig): void
     {
         $config = $this->configuration->getConfig();
 
@@ -78,19 +66,98 @@ class ExpectedDeliveryTest extends \PHPUnit\Framework\TestCase
         }
     }
 
-    public function dataProvider()
+    public static function dataProvider(): array
     {
-        include __DIR__ . '/../../_files/expected_delivery_scenarios.php';
-
-        return $scenarios;
+        return [
+            [
+                [
+                    'working_days' => '1,2,3,4,5',
+                    'holidays' => '14.03.2018
+            19.03.2018',
+                    'delivery_today_time' => '15:00',
+                    'default_shipping_time' => 2,
+                    'timestamp' => 1521201600,
+                    'utc_offset' => 0
+                ],
+                'simple_product',
+                ['shipDayName' => 'Wednesday', 'nextShipDayName' => 'Thursday']
+            ],
+            [
+                [
+                    'working_days' => '1,2,3,4,5',
+                    'holidays' => '14.03.2018
+            19.03.2018',
+                    'delivery_today_time' => '15:00',
+                    'default_shipping_time' => 2,
+                    'timestamp' => 1521374400,
+                    'utc_offset' => 0
+                ],
+                'simple_product',
+                ['shipDayName' => 'Wednesday', 'nextShipDayName' => 'Thursday']
+            ],
+            [
+                [
+                    'working_days' => '1,2,3,4,5',
+                    'holidays' => '21.03.2018
+            22.03.2018
+            25.03.2018',
+                    'delivery_today_time' => '15:00',
+                    'default_shipping_time' => 2,
+                    'timestamp' => 1521374400,
+                    'utc_offset' => 0
+                ],
+                'simple_product',
+                ['shipDayName' => 'Tuesday', 'nextShipDayName' => 'Friday']
+            ],
+            [
+                [
+                    'working_days' => '1,2,3,4,5',
+                    'holidays' => '22.03.2018
+            23.03.2018
+            25.03.2018',
+                    'delivery_today_time' => '15:00',
+                    'default_shipping_time' => 2,
+                    'timestamp' => 1521374400,
+                    'utc_offset' => 0
+                ],
+                'out_of_stock',
+                null
+            ],
+            [
+                [
+                    'working_days' => '1,2,3,4,5',
+                    'holidays' => '14.03.2018
+            19.03.2018',
+                    'delivery_today_time' => '15:00',
+                    'default_shipping_time' => 2,
+                    'timestamp' => 1521201600,
+                    'utc_offset' => 0
+                ],
+                'custom_product',
+                ['shipDayName' => 'Friday', 'nextShipDayName' => 'Monday']
+            ],
+            [
+                [
+                    'working_days' => '0,1,2,3,4,5,6',
+                    'holidays' => '14.03.2018
+            19.03.2018',
+                    'delivery_today_time' => '15:00',
+                    'default_shipping_time' => 2,
+                    'timestamp' => 1521201600,
+                    'utc_offset' => 0
+                ],
+                'custom_product',
+                ['shipDayName' => 'Wednesday', 'nextShipDayName' => 'Thursday']
+            ],
+        ];
     }
 
-    public static function loadProducts()
+    public static function loadProducts(): void
     {
         require __DIR__ . '/../../_files/expected_delivery_products.php';
     }
 
-    public static function loadProductsRollback()
+    public static function loadProductsRollback(): void
     {
         require __DIR__ . '/../../_files/expected_delivery_products_rollback.php';
     }
