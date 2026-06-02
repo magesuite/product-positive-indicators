f5fb0fc1fcfcc6c104223c0f3af6cc908275054d
Zbigniew Kuraś <zbigniew.kuras@creativestyle.pl>
2026-06-02 09:21:54 +0200

    feat: MGS-5727 remove trailing whitespace

--- Console/Command/TopAttributeRefresh.php
+++ Console/Command/TopAttributeRefresh.php
@@ -43,7 +43,7 @@ class TopAttributeRefresh extends \Symfony\Component\Console\Command\Command
         $event = $stopwatch->stop('calculate_top_attribute');
         $output->writeln('Duration: ' . $event->getDuration(). 'ms');
         $output->writeln('Max memory usage: ' . $event->getMemory()/1024/1024/8 . 'MB');
-       
+
         return $returnStatus;
     }
 }
